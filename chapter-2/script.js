9e7a3af435b8e7f8631d1fea254b2ba8e745e8cc
Ammar12302005 <155043025+Ammar12302005@users.noreply.github.com>
2024-02-23 23:37:56 +0500

    chapter 2 added

---
+++ chapter-2/script.js
@@ -0,0 +1,48 @@
+// Q1+2
+var username = "Q1 \n  Syed Ammar Ali"
+// alert ( )
+
+
+
+// Q3
+var message = "Q3 \n hello world "
+alert ( message)
+
+
+// Q4
+var StudentNAme = "Q4 \n Jhone Doe "
+var Age = "Q4 \n 18"
+var course = "Q4 \n Certified mobile app Development "
+alert (StudentNAme )
+alert (Age)
+alert (course)
+
+
+
+// Q5
+var pizza = "Q5 \n  pizza \n pizz \n piz \n pi \n p"
+alert ( pizza )
+
+
+// Q6
+var email = " example@example.com"
+alert ("Q6 \n my email address is" + email)
+
+
+// Q7
+var book = "A smarter way to learn Javascript "
+alert ("Q7 \n I am trying to learn from book, " +book)
+
+
+// Q8
+document.write (" Yeah! I can write HTML content trough Javascript ")
+
+
+// Q9
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert ( design)
+
+
+
+
+
