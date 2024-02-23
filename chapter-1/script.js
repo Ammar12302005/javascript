70c53a7c565ee15597cabde75e59892839c5d4a6
Ammar12302005 <155043025+Ammar12302005@users.noreply.github.com>
2024-02-23 23:37:42 +0500

    chapter 1 add

---
+++ chapter-1/script.js
@@ -0,0 +1,18 @@
+// Q1
+var java = "Q1 \n hello world"
+alert ( java)
+
+
+// Q2
+alert (' Q2 \n Erorr! plese Enter a valid password')
+
+
+
+// Q3
+alert ('Q3 \n Welcome to JS land \n Happy coding')
+
+
+
+// Q4
+alert ('Q4 \n welcome to js land')
+alert ("Happy coding")
