b7870046d0f10824659f7b0413ffe511835ac326
Ammar12302005 <155043025+Ammar12302005@users.noreply.github.com>
2024-02-24 00:23:10 +0500

    chapter 3 answers added

---
+++ chapter-3/script.js
@@ -0,0 +1,36 @@
+// Q1
+var age = "18"
+alert ("I am "+ age + "years old")
+
+
+// Q2
+alert ("you have visit this site 1 time")
+
+
+
+// Q3
+var BirthYear = "2005"
+document.write ("Q  my birth year is" + BirthYear + "times.<br>")
+
+
+
+// Q4
+var  name = "John" 
+var  product = "T-Shirts"
+var  quantity = "5" 
+document.write (name +"ordered "+ quantity + product + "on XYZ clothing store ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
