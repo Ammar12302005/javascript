cfddcaf38135b9f41fc87648457bbb22f53b100f
Ammar12302005 <155043025+Ammar12302005@users.noreply.github.com>
2024-02-24 01:34:00 +0500

    chap 4 answers

---
+++ chapter-4/script.js
@@ -0,0 +1,41 @@
+// var one = "I am Learning "
+// var two = "Javascript"
+// var three = " By Sir Bilal"
+// document.write (one+two+three)
+
+// Q1
+var Student1 = "ammar", Student2 = "ahad", Student3 = "ahmed";
+document.write(Student1 + "<br>" + Student2 + "<br>" + Student3 + "<br>");
+
+
+// Q2
+// legal
+let name = "John";
+let myName = "John";
+let _name = "John";
+let $name = "John";
+let name1 = "John";
+
+// illegal
+// let 1name = "John"; // starts with a number
+// let my-name = "John"; // contains a hyphen
+// let if = "John"; // is a reserved keyword
+// let class = "John"; // is a reserved keyword
+// let function = "John"; // is a reserved keyword
+
+document.write(
+  name + myName+ _name+ $name+ name1 + "times.<br>")
+document.write("1name + my-name+ if+ class+ function")
+
+
+
+
+
+
+
+
+
+
+
+
+
